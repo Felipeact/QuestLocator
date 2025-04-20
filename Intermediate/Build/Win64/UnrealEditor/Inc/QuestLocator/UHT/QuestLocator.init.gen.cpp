a4a1db0c2294b3a546ecf4737fd2a002de9c2efd
Felipe Viana <felipetiburcioviana@gmail.com>
2025-04-20 10:28:57 -0700

    Add project files.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/QuestLocator/UHT/QuestLocator.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeQuestLocator_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_QuestLocator;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_QuestLocator()
+	{
+		if (!Z_Registration_Info_UPackage__Script_QuestLocator.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/QuestLocator",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xC3D67AF3,
+				0x1CB861F8,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_QuestLocator.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_QuestLocator.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_QuestLocator(Z_Construct_UPackage__Script_QuestLocator, TEXT("/Script/QuestLocator"), Z_Registration_Info_UPackage__Script_QuestLocator, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xC3D67AF3, 0x1CB861F8));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
